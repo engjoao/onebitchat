05ec4ec839317baafce161eeb8e61b4fc9138799
João Alberto <aquinosilva@gmail.com>
2017-06-20 22:06:16 -0300

    Create coofescripts

--- app/controllers/team_users_controller.rb
+++ app/controllers/team_users_controller.rb
@@ -1,4 +1,3 @@
-
 class TeamUsersController < ApplicationController
   before_action :set_team_user, only: [:destroy]
 
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,22 +1,29 @@
-class TeamUsersController < ApplicationController
-  before_action :set_team_user, only: [:destroy]
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:destroy]
+  before_action :set_by_slug_team, only: [:show]
+
+  def index
+  end
+
+  def show
+    authorize! :read, @team
+  end
 
   def create
-    @team_user = TeamUser.new(team_user_params)
-    authorize! :create, @team_user
+    @team = Team.new(team_params)
 
     respond_to do |format|
-      if @team_user.save
-        format.json { render :show, status: :created }
+      if @team.save
+        format.html { redirect_to "/#{@team.slug}" }
       else
-        format.json { render json: @team_user.errors, status: :unprocessable_entity }
+        format.html { redirect_to main_app.root_url, notice: @team.errors }
       end
     end
   end
 
   def destroy
-    authorize! :destroy, @team_user
-    @team_user.destroy
+    authorize! :destroy, @team
+    @team.destroy
 
     respond_to do |format|
       format.json { render json: true }
@@ -25,12 +32,15 @@ class TeamUsersController < ApplicationController
 
   private
 
-  def set_team_user
-    @team_user = TeamUser.find_by(user_id: params[:id], team_id: params[:team_id])
+  def set_by_slug_team
+    @team = Team.find_by(slug: params[:slug])
   end
 
-  def team_user_params
-    user = User.find_by(email: params[:team_user][:email])
-    params.require(:team_user).permit(:team_id).merge(user_id: user.id)
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:slug).merge(user: current_user)
   end
 end
